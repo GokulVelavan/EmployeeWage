a9edeeaee9719646dc2021b2e5d56e931a7cb53e
GokulVelavan <gokulvelavans@gmail.com>
2021-10-10 19:28:33 +0530

    Wage for 20 working Days

--- Program.cs
+++ Program.cs
@@ -8,31 +8,42 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if(empCheck==IS_PART_TIME)
-            {
-                empHrs = 4;
-            }else if(empCheck==IS_FULL_TIME)
-            {
-                empHrs = 8;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("EMP WAGE:{0}", empWage);
             }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EMP WAGE:{0}", empWage);
+            Console.WriteLine(" Total EMP WAGE:{0}", totalEmpWage);
             Console.ReadLine();
+            }
         }
-    }
 }
