ae774daa3d721994f70e20f84544907a04ba3f62
GokulVelavan <gokulvelavans@gmail.com>
2021-10-10 18:43:56 +0530

    Employee Attendence

--- Program.cs
+++ Program.cs
@@ -11,22 +11,18 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
             Random random = new Random();
-
             int empCheck = random.Next(0, 2);
             if(empCheck==IS_FULL_TIME)
             {
-                empHrs = 8;
+                Console.WriteLine("Present");
             }
             else
             {
-                empHrs = 0;
+                Console.WriteLine("Absent");
+
+
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EMP Wage:{0}", empWage);
             Console.ReadLine();
         }
     }
