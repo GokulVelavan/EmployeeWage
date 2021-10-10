a0488e0f910e8d657c9f930a8ab628f6f1d01bbf
GokulVelavan <gokulvelavans@gmail.com>
2021-10-10 20:01:30 +0530

    20 working days

--- Program.cs
+++ Program.cs
@@ -8,23 +8,17 @@ namespace EmployeeWage
 {
     class Program
     {
-<<<<<<< HEAD
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
 
-=======
-         public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
->>>>>>> ee2b01351322d8c40a3c249bb3e42f269b7c3a1a
+
         static void Main(string[] args)
         {
 
             int empHrs = 0;
             int empWage = 0;
-<<<<<<< HEAD
             int totalEmpWage = 0;
             for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
@@ -52,28 +46,7 @@ namespace EmployeeWage
 
 
             Console.WriteLine(" Total EMP WAGE:{0}", totalEmpWage);
-=======
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-           switch (empCheck)
-            {
-                case IS_PART_TIME:
-                empHrs = 4;
-                break;
-
-                case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-
-                default:
-                empHrs = 0;
-                    break;
-            }
-            
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EMP WAGE:{0}", empWage);
->>>>>>> ee2b01351322d8c40a3c249bb3e42f269b7c3a1a
             Console.ReadLine();
             }
         }
